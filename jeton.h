38a0c1b75e8a7851c536bd125e58a7b1e2bd7f1b
Elora9 <elora.landreau50@gmail.com>
2025-03-31 13:05:37 +0200

    elora2

--- jeton.h
+++ jeton.h
@@ -1,51 +1,16 @@
-
-#ifndef JETON_H
-#define JETON_H
-
-
-
-//énumération des différents types de lexems existants
-typedef enum
-    {
-    REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
-    }typelexem;
-//énumération des différents types d'opérateurs existants
-typedef enum
-    {
-    PLUS,MOINS,FOIS,DIV,PUIS
-    }typeoperateur;
-//énumération des différents types de fonctions existantes
-typedef enum
-    {
-    ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
-    }typefonction;
-//énumération des différents types d'erreurs existantes
-typedef enum
-    {
-        E101,E102,E103,E104,E201,E202,E203,E301,E302
-    }typeerreur;
-////énumération des différents types de valeurs existantes
-typedef union
-    {
-    float reel;
-    char var;
-    typefonction fonction;
-    typeoperateur operateur;
-    typeerreur erreur;
-    }typevaleur;
-//énumération des différents types de jetons existants
-typedef struct 
-    {
-    typelexem lexem;
-    typevaleur valeur;
-    }typejeton;
-//récupération de l'arbre
-typedef struct Node
+#ifndef PatoisDuSud
+#define PatoisDuSud
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+struct jeton
 {
-    typejeton jeton;
-    struct Node *fg;
-    struct Node *fd;
-} Node;
-typedef Node *Arbre;
+    char type[10];
+    char valeur[15];
+    int longueur;
+};
 
+typedef struct jeton token;
+token lire_carac(char a,char fonction[100],int index);
 #endif
