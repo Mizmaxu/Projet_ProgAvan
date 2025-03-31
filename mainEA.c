9a597c401a6e5d978d81082135f9c9010f93602a
Elora9 <elora.landreau50@gmail.com>
2025-03-31 13:05:11 +0200

    elora1

--- mainEA.c
+++ mainEA.c
@@ -1,24 +1,35 @@
 #include "jeton.h"
 #include "syntaxique.h"
 
-typejeton creerjeton(typelexem lexem,typevaleur valeur);
-typejeton creerjeton(typelexem lexem,typevaleur valeur){
-    typejeton Jeton ;
-    Jeton.lexem = lexem;
-    Jeton.valeur = valeur;
+struct jeton creerjeton(char *type,char *valeur, int longueur){
+    struct jeton Jeton ;
+    strcpy(Jeton.type,type);
+    strcpy(Jeton.valeur,valeur);
+    Jeton.longueur = longueur;
     return Jeton;
 }
 
 
+
 int main(int argc, char** argv){
-    typejeton T[] = {creerjeton(FONCTION,(typevaleur){ .fonction = SIN }),
-    creerjeton(PAR_OUV,(typevaleur){ .var = '(' }),
-    creerjeton(VARIABLE,(typevaleur){ .var = 'x' }),
-    creerjeton(OPERATEUR, (typevaleur){ .operateur = PLUS}),
-    creerjeton(REEL,(typevaleur){.reel = 2}),
-    creerjeton(PAR_FERM,(typevaleur){ .var = ')' })} ;
-
-    afficheArbre(synth(T,0));
-
+    struct jeton T[] = {
+        creerjeton("FONCTION","sin",3),
+        creerjeton("PAR_OUV","(",1 ),
+        creerjeton("VARIABLE","x",1 ),
+        creerjeton("OPERATEUR", "PLUS",1),
+        creerjeton("REEL","2",1),
+        creerjeton("PAR_FERM",")",1),
+        creerjeton("OPERATEUR", "PLUS",1),
+        creerjeton("FONCTION","cos",3),
+        creerjeton("PAR_OUV","(",1 ),
+        creerjeton("VARIABLE","x",1 ),
+        creerjeton("PAR_FERM",")",1),
+        creerjeton("FIN","",0)} ;
+    char * err = erreur(T);
+    printf("%s",err);
+    if (strcmp(err,"")== 0){
+        int index = 0;
+        afficheArbre(synth(T,&index,taille_tab(T)));
+    }
     return 0;
 }
