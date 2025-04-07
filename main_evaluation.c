99d8931ebb84ff82bd6d46ce83b08793656bfd7b
Clovis <clovisbocq@gmail.com>
2025-04-07 15:34:10 +0200

    Update main_evaluation.c

--- main_evaluation.c
+++ main_evaluation.c
@@ -3,38 +3,33 @@
 #include <stdlib.h>
 
 /* 
-   On suppose que la structure 'jeton' est définie dans "syntaxique.h" comme suit :
-   
-   typedef struct jeton {
-       char *type;
-       char *valeur;
-   } jeton;
-   
-   Dans cet exemple, on fais l'expression (X + 3) * 2.
+   Dans cet exemple, on fais l'expression sin(X + 3) * 2.
 */
 
 int main(void) {
     // Création des jetons pour chaque élément de l'expression
-    struct jeton varJeton = {"VARIABLE", "X"};        // Représente la variable X
-    struct jeton threeJeton = {"REEL", "3"};            // Représente le nombre 3
-    struct jeton plusJeton = {"OPERATEUR", "PLUS"};       // Représente l'opérateur +
-    struct jeton twoJeton = {"REEL", "2"};              // Représente le nombre 2
-    struct jeton multiplyJeton = {"OPERATEUR", "FOIS"};   // Représente l'opérateur *
+    struct jeton sinJeton = {"FONCTION", "sin"};
+    struct jeton varJeton = {"VARIABLE", "X"};        //  la variable X
+    struct jeton threeJeton = {"REEL", "3"};            //  le nombre 3
+    struct jeton plusJeton = {"OPERATEUR", "PLUS"};       //  l'opérateur +
+    struct jeton twoJeton = {"REEL", "2"};              //  le nombre 2
+    struct jeton multiplyJeton = {"OPERATEUR", "FOIS"};   //  l'opérateur *
 
-    // Construction de l'arbre binaire de l'expression (X + 3) * 2
+    // Construction de l'arbre binair
+    
     Node xNode = {varJeton, NULL, NULL};
     Node threeNode = {threeJeton, NULL, NULL};
     Node plusNode = {plusJeton, &xNode, &threeNode};
+    Node sinNode = {sinJeton,&plusNode,NULL};
     Node twoNode = {twoJeton, NULL, NULL};
-    Node multiplyNode = {multiplyJeton, &plusNode, &twoNode};
+    Node multiplyNode = {multiplyJeton, &sinNode, &twoNode};
 
     // Définition de la valeur de X (exemple : X = 5)
-    float x = 8;
+    float x = 50;
     float resultat = evaluate(&multiplyNode, x);
 
     printf("Résultat pour X = %.2f : %.2f\n", x, resultat);
 
     return 0;
 }
-
 
